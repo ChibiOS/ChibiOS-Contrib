ed9baddee35122aa2654f3178148fee0d65b9547
marcoveeneman <marco-veeneman@hotmail.com>
2014-10-13 22:58:53 +0200

    Added Tiva board files.

---
+++ os/hal/boards/TI_TM4C1294_LAUNCHPAD/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/community/os/hal/boards/TI_TM4C1294_LAUNCHPAD/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/community/os/hal/boards/TI_TM4C1294_LAUNCHPAD
