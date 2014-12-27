82b8855e83fe93bd4f7857169d519811d17a6534
barthess <barthess@yandex.ru>
2014-12-27 22:14:50 +0300

    Added board files for ST_STM32F0308_DISCOVERY board

---
+++ os/hal/boards/ST_STM32F0308_DISCOVERY/board.mk
@@ -0,0 +1,5 @@
+# List of all the board related files.
+BOARDSRC = ${CHIBIOS}/community/os/hal/boards/ST_STM32F0308_DISCOVERY/board.c
+
+# Required include directories
+BOARDINC = ${CHIBIOS}/community/os/hal/boards/ST_STM32F0308_DISCOVERY
