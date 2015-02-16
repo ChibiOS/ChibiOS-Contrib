59499bd0cf2a1b1f604969207d9d488943bf798d
marcoveeneman <marco-veeneman@hotmail.com>
2015-02-16 21:31:04 +0100

    Added MAC low level driver for the TM4C129x. Tested on the TM4C1294 Connected Launchpad.

--- os/hal/ports/TIVA/TM4C129x/platform.mk
+++ os/hal/ports/TIVA/TM4C129x/platform.mk
@@ -3,7 +3,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/TM4C129x/hal_lld.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/st_lld.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/pal_lld.c \
-              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/serial_lld.c
+              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/serial_lld.c \
+              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/mac_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
