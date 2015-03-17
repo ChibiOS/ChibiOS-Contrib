7a68c44dd0c92c551604ad54619abafcc2477afa
marcoveeneman <marco-veeneman@hotmail.com>
2015-03-17 21:49:01 +0100

    Added Tiva UDMA initialization in hal_lld of TM4C123x devices. Added UDMA and SPI driver to platform.mk of TM4C123x devices.

--- os/hal/ports/TIVA/TM4C123x/platform.mk
+++ os/hal/ports/TIVA/TM4C123x/platform.mk
@@ -6,7 +6,9 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/serial_lld.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/i2c_lld.c \
               ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/gpt_lld.c \
-              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/pwm_lld.c
+              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/pwm_lld.c \
+              ${CHIBIOS}/community/os/hal/ports/TIVA/LLD/spi_lld.c \
+              ${CHISIOS}/community/os/hal/ports/TIVA/LLD/tiva_udma.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
