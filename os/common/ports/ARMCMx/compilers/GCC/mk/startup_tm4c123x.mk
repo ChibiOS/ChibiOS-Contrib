9d14e38afea0e28e66deb5abe182751b802ceed9
marcoveeneman <marco-veeneman@hotmail.com>
2015-04-15 23:16:23 +0200

    Tiva. Added new startup files.

---
+++ os/common/ports/ARMCMx/compilers/GCC/mk/startup_tm4c123x.mk
@@ -0,0 +1,10 @@
+# List of the ChibiOS generic TM4C123x startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt1.c \
+             $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c
+          
+STARTUPASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v7m.s
+
+STARTUPINC = $(CHIBIOS)/community/os/common/ports/ARMCMx/devices/TM4C123x \
+             $(CHIBIOS)/os/ext/CMSIS/include
+
+STARTUPLD  = $(CHIBIOS)/community/os/common/ports/ARMCMx/compilers/GCC/ld
