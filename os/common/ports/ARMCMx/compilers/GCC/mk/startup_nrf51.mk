203bb92de1b57af6c88db33882275135424badd7
Fabio Utzig <utzig@utzig.org>
2015-04-29 20:08:16 -0300

    Add statup makefile

---
+++ os/common/ports/ARMCMx/compilers/GCC/mk/startup_nrf51.mk
@@ -0,0 +1,11 @@
+# List of the ChibiOS generic NRF51 startup and CMSIS files.
+STARTUPSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt1.c \
+             $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c
+
+STARTUPASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v6m.s
+
+STARTUPINC = $(CHIBIOS)/community/os/common/ports/ARMCMx/devices/NRF51822 \
+             $(CHIBIOS)/os/ext/CMSIS/include
+
+STARTUPLD  = $(CHIBIOS)/community/os/common/ports/ARMCMx/compilers/GCC/ld
+
