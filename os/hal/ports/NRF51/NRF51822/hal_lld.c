5afd99de17e294d130b426aff43aeef962233415
Fabio Utzig <utzig@utzig.org>
2015-05-12 22:06:28 -0300

    Add basic HAL/ST drivers

---
+++ os/hal/ports/NRF51/NRF51822/hal_lld.c
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2015 Fabio Utzig
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+/**
+ * @file    NRF51/NRF51822/hal_lld.c
+ * @brief   NRF51822 HAL Driver subsystem low level driver source.
+ *
+ * @addtogroup HAL
+ * @{
+ */
+
+#include "hal.h"
+
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver interrupt handlers.                                                */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
+
+/**
+ * @brief   Low level HAL driver initialization.
+ *
+ * @notapi
+ */
+void hal_lld_init(void)
+{
+  // XXX: LED at PIN0.20 was initialized at __early_init
+
+  NRF_GPIO->PIN_CNF[18] = 1;
+  NRF_GPIO->PIN_CNF[19] = 1;
+  NRF_GPIO->OUTSET = ((uint32_t) 1 << 18) | ((uint32_t) 1 << 19);
+
+  //FIXME!
+  while (1) {}
+}
+
+/**
+ * @}
+ */
