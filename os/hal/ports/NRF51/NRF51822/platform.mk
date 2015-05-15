4ac0b638b92db47625d729b938e71b262dadbb43
Fabio Utzig <utzig@utzig.org>
2015-05-15 20:44:03 -0300

    Add basic PAL driver

--- os/hal/ports/NRF51/NRF51822/platform.mk
+++ os/hal/ports/NRF51/NRF51822/platform.mk
@@ -1,6 +1,7 @@
 # List of all the NRF51x platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS}/community/os/hal/ports/NRF51/NRF51822/hal_lld.c \
+              ${CHIBIOS}/community/os/hal/ports/NRF51/NRF51822/pal_lld.c \
               ${CHIBIOS}/community/os/hal/ports/NRF51/NRF51822/serial_lld.c \
               ${CHIBIOS}/community/os/hal/ports/NRF51/NRF51822/st_lld.c
 
