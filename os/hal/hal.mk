0229440fba26f4f1d1d34f6ff28e5a4b1b92a281
Andrea Zoppi <texzk@email.it>
2015-06-27 18:34:23 +0200

    ILI9341 driver moved to devices_lib

--- os/hal/hal.mk
+++ os/hal/hal.mk
@@ -1,8 +1,8 @@
 include ${CHIBIOS}/os/hal/hal.mk
 
 HALSRC += ${CHIBIOS}/community/os/hal/src/hal_community.c \
-	      ${CHIBIOS}/community/os/hal/src/nand.c \
-	      ${CHIBIOS}/community/os/hal/src/onewire.c \
-	      ${CHIBIOS}/community/os/hal/src/eicu.c
+          ${CHIBIOS}/community/os/hal/src/nand.c \
+          ${CHIBIOS}/community/os/hal/src/onewire.c \
+          ${CHIBIOS}/community/os/hal/src/eicu.c
 
 HALINC += ${CHIBIOS}/community/os/hal/include
