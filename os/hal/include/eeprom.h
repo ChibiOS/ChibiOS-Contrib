da01930ad0b191394b55bd460d12d78cf10cc05d
Fabien Poussin <fabien.poussin@gmail.com>
2016-02-04 12:01:15 +0100

    Inclusion fix for the eeprom driver.

--- os/hal/include/eeprom.h
+++ os/hal/include/eeprom.h
@@ -173,5 +173,8 @@ int eepfs_geterror(void *ip);
 msg_t eepfs_put(void *ip, uint8_t b);
 msg_t eepfs_get(void *ip);
 
+#include "ee24xx.h"
+#include "ee25xx.h"
+
 #endif /* #if defined(HAL_USE_EEPROM) && HAL_USE_EEPROM */
 #endif /* __EEPROM_H__ */
