9028916e8b89dcf9f8b41cefbd075efdf80cbac6
Fabien Poussin <fabien.poussin@gmail.com>
2016-02-16 00:22:40 +0100

    EEPROM: Initial commit

--- os/hal/hal.mk
+++ os/hal/hal.mk
@@ -11,6 +11,9 @@ HALSRC += ${CHIBIOS_CONTRIB}/os/hal/src/hal_community.c \
           ${CHIBIOS_CONTRIB}/os/hal/src/usbh/usbh_hub.c \
           ${CHIBIOS_CONTRIB}/os/hal/src/usbh/usbh_msd.c \
           ${CHIBIOS_CONTRIB}/os/hal/src/usbh/usbh_ftdi.c \
-          ${CHIBIOS_CONTRIB}/os/hal/src/usbh/usbh_uvc.c 
+          ${CHIBIOS_CONTRIB}/os/hal/src/usbh/usbh_uvc.c \
+	  ${CHIBIOS_CONTRIB}/os/hal/src/ee24xx.c \
+          ${CHIBIOS_CONTRIB}/os/hal/src/ee25xx.c \
+          ${CHIBIOS_CONTRIB}/os/hal/src/eeprom.c
 
 HALINC += ${CHIBIOS_CONTRIB}/os/hal/include
