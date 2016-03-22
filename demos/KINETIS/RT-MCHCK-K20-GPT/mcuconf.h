ac8960f1a83ea19645ca969e6c525defd8f47ff7
flabbergast <s3+flabbergast@sdfeu.org>
2016-03-22 20:14:43 +0000

    [KINETIS] Update demos (for HAL changes).

--- demos/KINETIS/RT-MCHCK-K20-GPT/mcuconf.h
+++ demos/KINETIS/RT-MCHCK-K20-GPT/mcuconf.h
@@ -32,27 +32,34 @@
 #define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
 #define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
 #define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
+#define KINETIS_CLKDIV1_OUTDIV1     1
+#define KINETIS_CLKDIV1_OUTDIV2     2
+#define KINETIS_CLKDIV1_OUTDIV4     2
+#define KINETIS_BUSCLK_FREQUENCY    (KINETIS_SYSCLK_FREQUENCY/KINETIS_CLKDIV1_OUTDIV2)
+#define KINETIS_FLASHCLK_FREQUENCY  (KINETIS_SYSCLK_FREQUENCY/KINETIS_CLKDIV1_OUTDIV4)
 
 /* FEE mode - 24 MHz with external 32.768 kHz crystal */
+/* not implemented */
 #if 0
 #define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
 #define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
 #define KINETIS_MCG_FLL_DRS         0           /* 732x FLL factor */
-#define KINETIS_MCG_FLL_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 24 MHz */
-#define KINETIS_MCG_FLL_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 12 MHz */
+#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 24 MHz */
+#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 12 MHz */
 #define KINETIS_SYSCLK_FREQUENCY    23986176UL  /* 32.768 kHz*732 (~24 MHz) */
 #define KINETIS_UART0_CLOCK_FREQ    (32768 * 732) /* FLL output */
 #define KINETIS_UART0_CLOCK_SRC     1           /* Select FLL clock */
-#define KINETIS_BUSCLK_FREQUENCY    (KINETIS_SYSCLK_FREQUENCY / KINETIS_MCG_FLL_OUTDIV4)
+#define KINETIS_BUSCLK_FREQUENCY    (KINETIS_SYSCLK_FREQUENCY / KINETIS_CLKDIV1_OUTDIV4)
 #endif /* 0 */
 
 /* FEE mode - 48 MHz */
+/* not implemented */
 #if 0
 #define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
 #define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
 #define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
-#define KINETIS_MCG_FLL_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 48 MHz */
-#define KINETIS_MCG_FLL_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 24 MHz */
+#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 48 MHz */
+#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 24 MHz */
 #define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
 #endif /* 0 */
 
@@ -72,7 +79,5 @@
  */
 #define KINETIS_GPT_USE_PIT0                TRUE
 #define KINETIS_GPT_PIT0_IRQ_PRIORITY       8
-/* TODO: Move this to a KINETIS registry */
-#define KINETIS_HAS_PIT0                    TRUE
 
 #endif /* _MCUCONF_H_ */
