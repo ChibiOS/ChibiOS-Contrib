3ba468b688b5858a9b09558aa287e68db0c0b87e
Fabio Utzig <utzig@utzig.org>
2016-04-04 20:14:12 -0300

    Fix paths in startup makefiles

--- os/common/ports/ARMCMx/compilers/GCC/mk/startup_tm4c129x.mk
+++ os/common/ports/ARMCMx/compilers/GCC/mk/startup_tm4c129x.mk
@@ -1,10 +1,11 @@
 # List of the ChibiOS generic TM4C129x startup and CMSIS files.
-STARTUPSRC = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt1.c \
-             $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/vectors.c
-          
-STARTUPASM = $(CHIBIOS)/os/common/ports/ARMCMx/compilers/GCC/crt0_v7m.s
+STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+             $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.c
 
-STARTUPINC = $(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/devices/TM4C129x \
-             $(CHIBIOS)/os/ext/CMSIS/include
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.s
+
+STARTUPINC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
+             $(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/devices/TM4C129x \
+             $(CHIBIOS)/os/common/ext/CMSIS/include
 
 STARTUPLD  = $(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/ld
