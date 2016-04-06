bae745fd33b11e9bf3d092d425bd5ec364759cdc
marcoveeneman <marco-veeneman@hotmail.com>
2016-04-06 21:33:16 +0200

    Tiva. WDG. Added watchdog driver to tm4c129 platform.mk.

--- os/hal/ports/TIVA/TM4C129x/platform.mk
+++ os/hal/ports/TIVA/TM4C129x/platform.mk
@@ -5,7 +5,8 @@ PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_pal_lld.c \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_serial_lld.c \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_mac_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_ext_lld.c
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_ext_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_wdg_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
