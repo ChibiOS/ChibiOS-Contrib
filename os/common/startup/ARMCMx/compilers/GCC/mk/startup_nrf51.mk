2789fb3cd85fe8b7cb67a77d6d676d8b84830de2
Stephane D'Alu <sdalu@sdalu.com>
2016-04-28 23:00:24 +0200

    sync with ChibiOS master for ASMXSRC

--- os/common/startup/ARMCMx/compilers/GCC/mk/startup_nrf51.mk
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_nrf51.mk
@@ -2,7 +2,7 @@
 STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
              $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/vectors.c
 
-STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.s
+STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v6m.S
 
 STARTUPINC = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/NRF51822 \
              $(CHIBIOS)/os/common/ext/CMSIS/include
