fab0f30fc033187bab94017f91203f2e05ced533
Nicolas Reinecke <nr@das-labor.org>
2016-05-15 19:55:04 +0200

    qei testhal: copyright

--- testhal/STM32/STM32F0xx/qei/main.c
+++ testhal/STM32/STM32F0xx/qei/main.c
@@ -1,5 +1,5 @@
 /*
-    ChibiOS - Copyright (C) 2006..2016 
+    ChibiOS - Copyright (C) 2006..2016 Nicolas Reinecke
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
