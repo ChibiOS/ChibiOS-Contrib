81c70f5ce6452f184e3cef46159ae2dc171bd51a
marcoveeneman <marco-veeneman@hotmail.com>
2016-07-06 20:12:03 +0200

    Organized Tiva LLD folder.

--- os/hal/ports/TIVA/TM4C123x/platform.mk
+++ os/hal/ports/TIVA/TM4C123x/platform.mk
@@ -1,18 +1,25 @@
 # List of all the TM4C123x platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/TM4C123x/hal_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_st_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_pal_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_serial_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_i2c_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_gpt_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_pwm_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_spi_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/tiva_udma.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_ext_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_wdg_lld.c
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO/hal_ext_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO/hal_pal_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM/hal_gpt_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM/hal_st_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/I2C/hal_i2c_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/PWM/hal_pwm_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/SSI/hal_spi_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/UART/hal_serial_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/uDMA/tiva_udma.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/WDT/hal_wdg_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/TM4C123x \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/I2C \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/PWM \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/SSI \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/UART \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/uDMA \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/WDT
--- os/hal/ports/TIVA/TM4C129x/platform.mk
+++ os/hal/ports/TIVA/TM4C129x/platform.mk
@@ -1,14 +1,27 @@
 # List of all the TM4C129x platform files.
 PLATFORMSRC = ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/TM4C129x/hal_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_st_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_pal_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_serial_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_mac_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_ext_lld.c \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/hal_wdg_lld.c
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO/hal_ext_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO/hal_pal_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM/hal_gpt_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM/hal_st_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/I2C/hal_i2c_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/MAC/hal_mac_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/PWM/hal_pwm_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/SSI/hal_spi_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/UART/hal_serial_lld.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/uDMA/tiva_udma.c \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/WDT/hal_wdg_lld.c
 
 # Required include directories
 PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
               ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/TM4C129x \
-              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPIO \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/GPTM \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/I2C \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/MAC \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/PWM \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/SSI \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/UART \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/uDMA \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/TIVA/LLD/WDT
