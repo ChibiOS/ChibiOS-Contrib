771c15111122c4f77a4861797fe2a38c57dba4a6
marcoveeneman <marco-veeneman@hotmail.com>
2016-08-24 20:39:47 +0200

    Added TivaWare folder to startup makefile.

--- os/common/startup/ARMCMx/compilers/GCC/mk/startup_tm4c129x.mk
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_tm4c129x.mk
@@ -6,6 +6,7 @@ STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S
 
 STARTUPINC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC \
              $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/TM4C129x \
-             $(CHIBIOS)/os/common/ext/CMSIS/include
+             $(CHIBIOS)/os/common/ext/CMSIS/include \
+             $(CHIBIOS_CONTRIB)/os/common/ext/TivaWare
 
 STARTUPLD  = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/ld
