5a876ba2b04ee0933ced272b04e2ee481e48a1b1
Diego Ismirlian <dismirlian (at) google's mail.com>
2017-06-06 15:25:05 -0300

    USBH: License change for UVC driver

--- os/hal/src/usbh/hal_usbh_uvc.c
+++ os/hal/src/usbh/hal_usbh_uvc.c
@@ -1,14 +1,18 @@
 /*
- * usbh_uvc.c
- *
- *  Created on: 14 de set. de 2015
- *      Author: Diego Ismirlian (dismirlian (at) google's mail (dot) com)
- *
- *  License:
- *
- *  This file is free for non-commercial use until the company I work for decides what to do.
- *  If in doubt, please contact me.
- *
+    ChibiOS - Copyright (C) 2006..2017 Giovanni Di Sirio
+              Copyright (C) 2015..2017 Diego Ismirlian, (dismirlian (at) google's mail)
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
  */
 
 #include "hal.h"
