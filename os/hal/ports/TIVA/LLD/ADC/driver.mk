801b264b754c056ddf31acdc98bd878527e99ccc
marcoveeneman <marco-veeneman@hotmail.com>
2017-07-03 23:03:37 +0200

    Fixed ADC driver.mk

--- os/hal/ports/TIVA/LLD/ADC/driver.mk
+++ os/hal/ports/TIVA/LLD/ADC/driver.mk
@@ -1,9 +1,9 @@
 ifeq ($(USE_SMART_BUILD),yes)
 ifneq ($(findstring HAL_USE_ADC TRUE,$(HALCONF)),)
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/TIVA/LLD/I2C/hal_adc_lld.c
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/TIVA/LLD/ADC/hal_adc_lld.c
 endif
 else
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/TIVA/LLD/I2C/hal_adc_lld.c
+PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/TIVA/LLD/ADC/hal_adc_lld.c
 endif
 
 PLATFORMINC += $(CHIBIOS_CONTRIB)/os/hal/ports/TIVA/LLD/ADC
