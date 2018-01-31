90b7d6bbd03deead3d6bd6c8e334d6dfdd195feb
Adrian <af@coda.pw>
2018-01-31 09:55:38 +0100

    Added support for STM32F7
    Tested only for STM32F746, other chipsets have to be checked.

--- os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc.h
+++ os/hal/ports/STM32/LLD/FSMCv1/hal_fsmc.h
@@ -36,7 +36,10 @@
  */
 #if (defined(STM32F427xx) || defined(STM32F437xx) || \
      defined(STM32F429xx) || defined(STM32F439xx) || \
-     defined(STM32F769xx))
+     defined(STM32F745xx) || defined(STM32F746xx) || \
+     defined(STM32F756xx) || defined(STM32F767xx) || \
+     defined(STM32F769xx) || defined(STM32F777xx) || \
+     defined(STM32F779xx))
   #if !defined(FSMC_Bank1_R_BASE)
   #define FSMC_Bank1_R_BASE               (FMC_R_BASE + 0x0000)
   #endif
