1fefdf78fef847c27d3768cc78de0ee901889465
Michael Walker <walkerstop@gmail.com>
2018-05-02 03:47:26 -0700

    Remove duplicate code already in upstream master and add include dir

--- os/common/startup/ARMCMx/compilers/GCC/mk/startup_nrf52.mk
+++ os/common/startup/ARMCMx/compilers/GCC/mk/startup_nrf52.mk
@@ -5,6 +5,7 @@ STARTUPSRC = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
 STARTUPASM = $(CHIBIOS)/os/common/startup/ARMCMx/compilers/GCC/crt0_v7m.S
 
 STARTUPINC = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/devices/NRF52832 \
-             $(CHIBIOS)/os/common/ext/CMSIS/include
+             $(CHIBIOS)/os/common/ext/CMSIS/include \
+             $(CHIBIOS)/os/common/ext/ARM/CMSIS/Core/Include
 
 STARTUPLD  = $(CHIBIOS_CONTRIB)/os/common/startup/ARMCMx/compilers/GCC/ld
