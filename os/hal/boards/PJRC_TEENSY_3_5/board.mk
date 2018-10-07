a6bb21d05b4dd7299de9a632cf55bda7c058164e
Konstantin Oblaukhov <oblaukhov.konstantin@gmail.com>
2018-10-07 18:37:15 +0700

    KINETIS tests and demos updated to ChibiOS 18.2.x.

--- os/hal/boards/PJRC_TEENSY_3_5/board.mk
+++ os/hal/boards/PJRC_TEENSY_3_5/board.mk
@@ -3,3 +3,7 @@ BOARDSRC = ${CHIBIOS_CONTRIB}/os/hal/boards/PJRC_TEENSY_3_5/board.c
 
 # Required include directories
 BOARDINC = ${CHIBIOS_CONTRIB}/os/hal/boards/PJRC_TEENSY_3_5
+
+# Shared variables
+ALLCSRC += $(BOARDSRC)
+ALLINC  += $(BOARDINC)
