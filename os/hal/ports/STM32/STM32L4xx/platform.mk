9483bacf8847278d5a32b0dafeb32c416b676df4
Fabien Poussin <fabien.poussin@gmail.com>
2019-09-17 13:51:40 +0200

    Adding devices for STM32L4

--- os/hal/ports/STM32/STM32L4xx/platform.mk
+++ os/hal/ports/STM32/STM32L4xx/platform.mk
@@ -13,11 +13,14 @@ else
 endif
 
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/CRCv1/driver.mk
+include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/COMPv1/driver.mk
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/DMA2Dv1/driver.mk
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/FSMCv1/driver.mk
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/LTDCv1/driver.mk
+include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/OPAMPv1/driver.mk
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/TIMv1/driver.mk
 include ${CHIBIOS_CONTRIB}/os/hal/ports/STM32/LLD/USBHv1/driver.mk
+
 
 # Shared variables
 ALLCSRC += $(PLATFORMSRC_CONTRIB)
