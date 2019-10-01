30e0c775b75f96b9ea8f89792b49e45215b2b595
Fabien Poussin <fabien.poussin@gmail.com>
2019-10-01 23:33:26 +0200

    Fixed STM32 demos

--- demos/STM32/RT-STM32F429-DISCOVERY-DMA2D/mcuconf_community.h
+++ demos/STM32/RT-STM32F429-DISCOVERY-DMA2D/mcuconf_community.h
@@ -42,3 +42,16 @@
 #define STM32_SRAM_USE_FSMC_SRAM2           FALSE
 #define STM32_SRAM_USE_FSMC_SRAM3           FALSE
 #define STM32_SRAM_USE_FSMC_SRAM4           FALSE
+
+/*
+ * LTDC driver system settings.
+ */
+#define STM32_LTDC_USE_LTDC                 TRUE
+#define STM32_LTDC_EV_IRQ_PRIORITY          11
+#define STM32_LTDC_ER_IRQ_PRIORITY          11
+
+/*
+ * DMA2D driver system settings.
+ */
+#define STM32_DMA2D_USE_DMA2D               TRUE
+#define STM32_DMA2D_IRQ_PRIORITY            11
