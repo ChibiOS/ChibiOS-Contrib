20369a301179fc31ed34944ae851271f12dbd091
codetector <codetector@codetector.cn>
2020-11-26 16:04:08 -0500

    [LPC11Uxx]: Initial Support: RT + HAL ClockInit

---
+++ os/hal/ports/LPC/LPC11Uxx/platform.mk
@@ -0,0 +1,13 @@
+# List of all the LPC11Uxx platform files.
+PLATFORMSRC := 	${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
+				${CHIBIOS_CONTRIB}/os/hal/ports/LPC/LPC11Uxx/hal_lld.c
+
+# Required include directories
+PLATFORMINC =	${CHIBIOS}/os/hal/ports/common/ARMCMx \
+						${CHIBIOS_CONTRIB}/os/hal/ports/LPC/LPC11Uxx
+
+include $(CHIBIOS_CONTRIB)/os/hal/ports/LPC/LLD/STM/driver.mk
+
+# Shared variables
+ALLCSRC += $(PLATFORMSRC)
+ALLINC  += $(PLATFORMINC)
