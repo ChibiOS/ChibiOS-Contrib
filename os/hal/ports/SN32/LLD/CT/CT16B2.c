7d24ca4114aa095619841db1987087145425e924
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-01-21 18:11:37 +0200

    sn32: handle all MRs in CT16

--- os/hal/ports/SN32/LLD/CT/CT16B2.c
+++ os/hal/ports/SN32/LLD/CT/CT16B2.c
@@ -143,6 +143,195 @@ __irq void CT16B2_IRQHandler(void)
 			SN_CT16B2->IC = mskCT16_MR3IC;	//Clear MR3 match interrupt status
 		}
 	}
+	//MR4
+	if (SN_CT16B1->MCTRL_b.MR4IE)				//Check if MR4 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR4IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR4IF;
+			SN_CT16B1->IC = mskCT16_MR4IC;	//Clear MR4 match interrupt status
+		}
+	}
+	//MR5
+	if (SN_CT16B1->MCTRL_b.MR5IE)				//Check if MR5 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR5IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR5IF;
+			SN_CT16B1->IC = mskCT16_MR5IC;	//Clear MR5 match interrupt status
+		}
+	}
+	//MR6
+	if (SN_CT16B1->MCTRL_b.MR6IE)				//Check if MR6 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR6IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR6IF;
+			SN_CT16B1->IC = mskCT16_MR6IC;	//Clear MR6 match interrupt status
+		}
+	}
+	//MR7
+	if (SN_CT16B1->MCTRL_b.MR7IE)				//Check if MR7 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR7IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR7IF;
+			SN_CT16B1->IC = mskCT16_MR7IC;	//Clear MR7 match interrupt status
+		}
+	}
+	//MR8
+	if (SN_CT16B1->MCTRL_b.MR8IE)				//Check if MR8 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR8IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR8IF;
+			SN_CT16B1->IC = mskCT16_MR8IC;	//Clear MR8 match interrupt status
+		}
+	}
+	//MR9
+	if (SN_CT16B1->MCTRL_b.MR9IE)				//Check if MR9 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR9IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR9IF;
+			SN_CT16B1->IC = mskCT16_MR9IC;	//Clear MR9 match interrupt status
+		}
+	}
+	//MR10
+	if (SN_CT16B1->MCTRL_b.MR10IE)				//Check if MR10 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR10IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR10IF;
+			SN_CT16B1->IC = mskCT16_MR10IC;	//Clear MR10 match interrupt status
+		}
+	}
+	//MR11
+	if (SN_CT16B1->MCTRL_b.MR11IE)				//Check if MR11 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR11IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR11IF;
+			SN_CT16B1->IC = mskCT16_MR11IC;	//Clear MR11 match interrupt status
+		}
+	}
+	//MR12
+	if (SN_CT16B1->MCTRL_b.MR12IE)				//Check if MR12 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR12IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR12IF;
+			SN_CT16B1->IC = mskCT16_MR12IC;	//Clear MR12 match interrupt status
+		}
+	}
+	//MR13
+	if (SN_CT16B1->MCTRL_b.MR13IE)				//Check if MR13 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR13IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR13IF;
+			SN_CT16B1->IC = mskCT16_MR13IC;	//Clear MR13 match interrupt status
+		}
+	}
+	//MR14
+	if (SN_CT16B1->MCTRL_b.MR14IE)				//Check if MR14 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR14IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR14IF;
+			SN_CT16B1->IC = mskCT16_MR14IC;	//Clear MR14 match interrupt status
+		}
+	}
+	//MR15
+	if (SN_CT16B1->MCTRL_b.MR15IE)				//Check if MR15 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR15IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR15IF;
+			SN_CT16B1->IC = mskCT16_MR15IC;	//Clear MR15 match interrupt status
+		}
+	}
+	//MR16
+	if (SN_CT16B1->MCTRL_b.MR16IE)				//Check if MR16 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR16IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR16IF;
+			SN_CT16B1->IC = mskCT16_MR16IC;	//Clear MR16 match interrupt status
+		}
+	}
+	//MR17
+	if (SN_CT16B1->MCTRL_b.MR17IE)				//Check if MR17 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR17IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR17IF;
+			SN_CT16B1->IC = mskCT16_MR17IC;	//Clear MR17 match interrupt status
+		}
+	}
+	//MR18
+	if (SN_CT16B1->MCTRL_b.MR18IE)				//Check if MR18 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR18IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR18IF;
+			SN_CT16B1->IC = mskCT16_MR18IC;	//Clear MR18 match interrupt status
+		}
+	}
+	//MR19
+	if (SN_CT16B1->MCTRL_b.MR19IE)				//Check if MR19 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR19IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR19IF;
+			SN_CT16B1->IC = mskCT16_MR19IC;	//Clear MR19 match interrupt status
+		}
+	}
+	//MR20
+	if (SN_CT16B1->MCTRL_b.MR20IE)				//Check if MR20 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR20IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR20IF;
+			SN_CT16B1->IC = mskCT16_MR20IC;	//Clear MR20 match interrupt status
+		}
+	}
+	//MR21
+	if (SN_CT16B1->MCTRL_b.MR21IE)				//Check if MR21 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR21IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR21IF;
+			SN_CT16B1->IC = mskCT16_MR21IC;	//Clear MR21 match interrupt status
+		}
+	}
+	//MR22
+	if (SN_CT16B1->MCTRL_b.MR22IE)				//Check if MR22 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR22IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR22IF;
+			SN_CT16B1->IC = mskCT16_MR22IC;	//Clear MR22 match interrupt status
+		}
+	}
+	//MR23
+	if (SN_CT16B1->MCTRL_b.MR23IE)				//Check if MR23 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR23IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR23IF;
+			SN_CT16B1->IC = mskCT16_MR23IC;	//Clear MR23 match interrupt status
+		}
+	}
+	//MR24
+	if (SN_CT16B1->MCTRL_b.MR24IE)				//Check if MR24 IE enables?
+	{
+		if(iwRisStatus & mskCT16_MR24IF)
+		{
+			iwCT16B1_IrqEvent |= mskCT16_MR24IF;
+			SN_CT16B1->IC = mskCT16_MR24IC;	//Clear MR24 match interrupt status
+		}
+	}
 	//CAP0
 	if (SN_CT16B2->CAPCTRL_b.CAP0IE)		//Check if CAP0 IE enables?
 	{
