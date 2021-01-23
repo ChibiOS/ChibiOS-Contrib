9aeb769a9c22d1f906fdd09e0c0b6c00b3015b46
Dimitris Mantzouranis <d3xter93@gmail.com>
2021-01-23 16:56:29 +0200

    sn32f240: align ISR mapping with the vector table

--- os/hal/ports/SN32/SN32F240/hal_ext_lld_isr.h
+++ os/hal/ports/SN32/SN32F240/hal_ext_lld_isr.h
@@ -37,95 +37,88 @@
  */
 
 /*
- * GPIO units.
+ * ST unit
  */
-
-#define SN32_GPIOAB_HANDLER        Vector50
-#define SN32_GPIOCDF_HANDLER       Vector54
-
-#define SN32_GPIOAB_NUMBER         GPAB_IRQn
-#define SN32_GPIOCDF_NUMBER        GPCDF_IRQn
+#define SN32_ST_HANDLER           Vector3C
+#define SN32_ST_NUMBER            SysTick_IRQn
 
 /*
- * Special ST unit
+ * NDT unit.
  */
-#define SN32_ST_HANDLER            Vector3C
-#define SN32_ST_NUMBER             SysTick_IRQn
+#define SN32_NDT_HANDLER          Vector40
+#define SN32_NDT_NUMBER           NDT_IRQn
 
 /*
- * DMA units.
+ * USB unit.
  */
-#define SN32_PDMA_HANDLER          VectorA8
-#define SN32_PDMA_NUMBER           PDMA_IRQn
+#define SN32_USB_HANDLER          Vector44
+#define SN32_USB_NUMBER           USB_IRQn
+
+#define USBD_INTSTS_EPEVT_Pos        USBD_INTSTS_EPEVT0_Pos
+#define USBD_INTSTS_EPEVT_Msk        (0xFFul << USBD_INTSTS_EPEVT_Pos)
 
 /*
- * ADC units.
+ * SPI unit.
  */
-#define SN32_ADC_HANDLER          VectorB0
-#define SN32_ADC_NUMBER           ADC_IRQn
-
-/*
- * PWM units.
- */
-#define SN32_PWMA_HANDLER         Vector58
-#define SN32_PWMA_NUMBER          PWMA_IRQn
-
-/*
- * SPI units.
- */
-#define SN32_SPI0_HANDLER          Vector78
-#define SN32_SPI1_HANDLER          Vector7C
-#define SN32_SPI2_HANDLER          Vector80
-
+#define SN32_SPI0_HANDLER          Vector58
 #define SN32_SPI0_NUMBER           SPI0_IRQn
-#define SN32_SPI1_NUMBER           SPI1_IRQn
-#define SN32_SPI2_NUMBER           SPI2_IRQn
 
 /*
- * I2S units.
+ * I2C unit.
  */
-#define SN32_I2C1_HANDLER         VectorB8
-#define SN32_I2C1_NUMBER          I2S_IRQn
-
-/*
- * I2C units.
- */
-#define SN32_I2C1_GLOBAL_HANDLER   Vector88
-#define SN32_I2C1_GLOBAL_NUMBER    I2C0_IRQn
-
-#define SN32_I2C2_GLOBAL_HANDLER   Vector8C
-#define SN32_I2C2_GLOBAL_NUMBER    I2C1_IRQn
-
-/*
- * TIM units.
- */
-#define SN32_TIM1_HANDLER          Vector60
-#define SN32_TIM2_HANDLER          Vector64
-#define SN32_TIM3_HANDLER          Vector68
-#define SN32_TIM4_HANDLER          Vector6C
-
-#define SN32_TIM1_NUMBER           TMR0_IRQn
-#define SN32_TIM2_NUMBER           TMR1_IRQn
-#define SN32_TIM3_NUMBER           TMR2_IRQn
-#define SN32_TIM4_NUMBER           TMR3_IRQn
+#define SN32_I2C0_GLOBAL_HANDLER   Vector68
+#define SN32_I2C0_GLOBAL_NUMBER    I2C0_IRQn
 
 /*
  * USART units.
  */
-#define SN32_USART1_HANDLER        Vector70
-#define SN32_USART2_HANDLER        Vector74
+#define SN32_USART0_HANDLER        Vector70
+#define SN32_USART1_HANDLER        Vector74
+#define SN32_USART2_HANDLER        Vector78
 
-#define SN32_USART1_NUMBER         UART0_IRQn
-#define SN32_USART2_NUMBER         UART1_IRQn
+#define SN32_USART0_NUMBER         UART0_IRQn
+#define SN32_USART1_NUMBER         UART1_IRQn
+#define SN32_USART2_NUMBER         UART2_IRQn
 
 /*
- * USB units.
+ * CT16 units.
  */
-#define SN32_USB1_HANDLER          Vector9C
-#define SN32_USB1_NUMBER           USBD_IRQn
+#define SN32_CT16B0_HANDLER       Vector7C
+#define SN32_CT16B1_HANDLER       Vector80
 
-#define USBD_INTSTS_EPEVT_Pos        USBD_INTSTS_EPEVT0_Pos
-#define USBD_INTSTS_EPEVT_Msk        (0xFFul << USBD_INTSTS_EPEVT_Pos)
+#define SN32_CT16B0_NUMBER        CT16B0_IRQn
+#define SN32_CT16B1_NUMBER        CT16B1_IRQn
+
+/*
+ * ADC unit.
+ */
+#define SN32_ADC_HANDLER          VectorA0
+#define SN32_ADC_NUMBER           ADC_IRQn
+
+/*
+ * WDT unit.
+ */
+#define SN32_WDT_HANDLER          VectorA4
+#define SN32_WDT_NUMBER           WDT_IRQn
+
+/*
+ * LVD unit.
+ */
+#define SN32_LVD_HANDLER          VectorA8
+#define SN32_LVD_NUMBER           LVD_IRQn
+
+/*
+ * GPIO units.
+ */
+#define SN32_GPIOD_HANDLER        VectorB0
+#define SN32_GPIOC_HANDLER        VectorB4
+#define SN32_GPIOB_HANDLER        VectorB8
+#define SN32_GPIOA_HANDLER        VectorBC
+
+#define SN32_GPIOD_NUMBER         P3_IRQn
+#define SN32_GPIOC_NUMBER         P2_IRQn
+#define SN32_GPIOB_NUMBER         P1_IRQn
+#define SN32_GPIOA_NUMBER         P0_IRQn
 
 /*===========================================================================*/
 /* Driver pre-compile time settings.                                         */
