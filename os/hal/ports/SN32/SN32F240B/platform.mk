7f2ba3dbfad31407736057e461730b5c1d0d8591
HorrorTroll <sonicvipduc@gmail.com>
2021-03-15 11:36:32 +0700

    fixed a bit for 240B, should work now

--- os/hal/ports/SN32/SN32F240B/platform.mk
+++ os/hal/ports/SN32/SN32F240B/platform.mk
@@ -1,10 +1,10 @@
 # Required platform files.
 PLATFORMSRC := $(CHIBIOS)/os/hal/ports/common/ARMCMx/nvic.c \
-               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F240/hal_lld.c
+               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F240B/hal_lld.c
 
 # Required include directories.
 PLATFORMINC := $(CHIBIOS)/os/hal/ports/common/ARMCMx \
-               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F240
+               $(CHIBIOS_CONTRIB)/os/hal/ports/SN32/SN32F240B
 
 # Optional platform files.
 ifeq ($(USE_SMART_BUILD),yes)
@@ -24,15 +24,15 @@ else
 endif
 
 # Drivers compatible with the platform.
-include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/GPIO/driver.mk
-include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/USB/driver.mk
-include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/CT/driver.mk
+include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/GPIO/driver.mk
+include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/USB/driver.mk
+include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/CT/driver.mk
 
 
-# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/TIM/driver.mk
-# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/GPIOv3/driver.mk
-# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/USBv1/driver.mk
-# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/TIMv1/driver.mk
+# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/TIM/driver.mk
+# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/GPIOv3/driver.mk
+# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/USBv1/driver.mk
+# include ${CHIBIOS_CONTRIB}/os/hal/ports/SN32/LLD/SN32F24xB/TIMv1/driver.mk
 
 # PLATFORMINC += $(CHIBIOS)/os/hal/templates/
 # PLATFORMSRC += ${CHIBIOS}/os/hal/templates/hal_usb_lld.c
