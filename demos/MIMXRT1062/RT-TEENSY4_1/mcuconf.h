b6636284af526f42cf108374b2b156ad516d103d
Michael Stapelberg <stapelberg@google.com>
2021-03-26 12:33:58 +0100

    demos/RT-TEENSY4_1: enable USB shell / tests in demo

--- demos/MIMXRT1062/RT-TEENSY4_1/mcuconf.h
+++ demos/MIMXRT1062/RT-TEENSY4_1/mcuconf.h
@@ -19,73 +19,8 @@
 
 #define MIMXRT1062_MCUCONF
 
-/*
- * HAL driver system settings.
- */
-
-/* Select the MCU clocking mode below by enabling the appropriate block. */
-
-/* PEE mode - 180 MHz system clock driving by 16 MHz xtal */
-#if 1
-#define KINETIS_MCG_MODE KINETIS_MCG_MODE_PEE
-#define KINETIS_PLLCLK_FREQUENCY 180000000UL
-#define KINETIS_SYSCLK_FREQUENCY KINETIS_PLLCLK_FREQUENCY
-#define KINETIS_BUSCLK_FREQUENCY 60000000UL
-#define KINETIS_FLASHCLK_FREQUENCY 28000000UL
-
-#define KINETIS_CLKDIV1_OUTDIV1 1  // -> 0
-#define KINETIS_CLKDIV1_OUTDIV2 3  // -> 2
-#define KINETIS_CLKDIV1_OUTDIV4 7  // -> 6
-#endif
-
-/* PEE mode - 48MHz system clock driven by external crystal. */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_PEE
-#define KINETIS_PLLCLK_FREQUENCY    96000000UL
-#define KINETIS_SYSCLK_FREQUENCY    48000000UL
-#endif
-
-/* FEI mode (~48MHz) */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEI
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
-#define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide MCGCLKOUT (~48MHz) by 1 to SYSCLK */
-#define KINETIS_CLKDIV1_OUTDIV2     1           /* Divide by 1 for (~48MHz) peripheral clock */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide by 2 for (~24MHz) flash clock */
-#define KINETIS_BUSCLK_FREQUENCY    KINETIS_SYSCLK_FREQUENCY
-#define KINETIS_FLASHCLK_FREQUENCY  KINETIS_SYSCLK_FREQUENCY/2
-#endif /* 0 */
-
-/* FEE mode - 24 MHz with external 32.768 kHz crystal */
-/* not implemented */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         0           /* 732x FLL factor */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 24 MHz */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 12 MHz */
-#define KINETIS_SYSCLK_FREQUENCY    23986176UL  /* 32.768 kHz*732 (~24 MHz) */
-#define KINETIS_UART0_CLOCK_FREQ    (32768 * 732) /* FLL output */
-#define KINETIS_UART0_CLOCK_SRC     1           /* Select FLL clock */
-#define KINETIS_BUSCLK_FREQUENCY    (KINETIS_SYSCLK_FREQUENCY / KINETIS_CLKDIV1_OUTDIV4)
-#endif /* 0 */
-
-/* FEE mode - 48 MHz */
-/* not implemented */
-#if 0
-#define KINETIS_MCG_MODE            KINETIS_MCG_MODE_FEE
-#define KINETIS_MCG_FLL_DMX32       1           /* Fine-tune for 32.768 kHz */
-#define KINETIS_MCG_FLL_DRS         1           /* 1464x FLL factor */
-#define KINETIS_CLKDIV1_OUTDIV1     1           /* Divide 48 MHz FLL by 1 => 48 MHz */
-#define KINETIS_CLKDIV1_OUTDIV4     2           /* Divide OUTDIV1 output by 2 => 24 MHz */
-#define KINETIS_SYSCLK_FREQUENCY    47972352UL  /* 32.768 kHz * 1464 (~48 MHz) */
-#endif /* 0 */
-
-/*
- * SERIAL driver system settings.
- */
-#define KINETIS_SERIAL_USE_UART0              TRUE
+/* The NXP USB stack uses more stack space than the default 256 byte of thread
+ * working area for the test command can fit, so make some more room: */
+#define SHELL_CMD_TEST_WA_SIZE              THD_WORKING_AREA_SIZE(1024)
 
 #endif /* _MCUCONF_H_ */
