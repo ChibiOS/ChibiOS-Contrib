f9ed7a6f056dd53980da7b8aefbcc66b9e56591e
Joy <chang.li@westberrytech.com>
2022-04-25 15:29:45 +0800

    Update driver.mk

--- os/hal/ports/WB32/LLD/UARTv1/driver.mk
+++ os/hal/ports/WB32/LLD/UARTv1/driver.mk
@@ -1,13 +1,13 @@
 ifeq ($(USE_SMART_BUILD),yes)
 ifneq ($(findstring HAL_USE_SERIAL TRUE,$(HALCONF)),)
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_serial_lld.c
+PLATFORMSRC_CONTRIB += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_serial_lld.c
 endif
 ifneq ($(findstring HAL_USE_UART TRUE,$(HALCONF)),)
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_uart_lld.c
+PLATFORMSRC_CONTRIB += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_uart_lld.c
 endif
 else
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_serial_lld.c
-PLATFORMSRC += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_uart_lld.c
+PLATFORMSRC_CONTRIB += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_serial_lld.c
+PLATFORMSRC_CONTRIB += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1/hal_uart_lld.c
 endif
 
 PLATFORMINC_CONTRIB += $(CHIBIOS_CONTRIB)/os/hal/ports/WB32/LLD/UARTv1
