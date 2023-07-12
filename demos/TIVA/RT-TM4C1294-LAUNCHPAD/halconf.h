8f022dbb1dac4c7a1d93de271c3b0b3100d50108
Alberto Panu <alberto@panu.it>
2023-07-12 17:19:20 +0200

    Update halconf.h
    Correct some issues

--- demos/TIVA/RT-TM4C1294-LAUNCHPAD/halconf.h
+++ demos/TIVA/RT-TM4C1294-LAUNCHPAD/halconf.h
@@ -29,7 +29,8 @@
 #define HALCONF_H
 
 #define _CHIBIOS_HAL_CONF_
-#define _CHIBIOS_HAL_CONF_VER_8_0_
+//#define _CHIBIOS_HAL_CONF_VER_8_0_
+#define _CHIBIOS_HAL_CONF_VER_8_4_
 
 #include "mcuconf.h"
 
@@ -210,7 +211,8 @@
  * @note    Disabling this option saves both code and data space.
  */
 #if !defined(PAL_USE_CALLBACKS) || defined(__DOXYGEN__)
-#define PAL_USE_CALLBACKS                   FALSE
+//#define PAL_USE_CALLBACKS                   FALSE
+#define PAL_USE_CALLBACKS                   TRUE
 #endif
 
 /**
@@ -526,7 +528,7 @@
 #define WSPI_USE_MUTUAL_EXCLUSION           TRUE
 #endif
 
-#include "halconf_community.h"
+//#include "halconf_community.h"
 
 #endif /* HALCONF_H */
 
