7e125c79e210c66806bf59258f2db2198379080f
Xael South <xael.south@yandex.com>
2024-06-02 21:30:48 +0200

    fixed gecko_sdk path

--- demos/SILABS/RT-EFR32FG23A010F256-BRD4001A-REVA01/emlib/emlib.mk
+++ demos/SILABS/RT-EFR32FG23A010F256-BRD4001A-REVA01/emlib/emlib.mk
@@ -1,32 +1,34 @@
 # Required platform files.
+GECKO_SDK = $(CHIBIOS_CONTRIB)/ext/gecko_sdk
+
 EMLIBSRC = \
                emlib/emlib_chibios.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/Device/SiliconLabs/EFR32FG23/Source/system_efr32fg23.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/common/src/sl_assert.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_acmp.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_cryotimer.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_cmu.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_emu.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_eusart.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_gpio.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_letimer.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_pcnt.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_prs.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_lesense.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_lcd.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_system.c \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/src/em_vdac.c
+               $(GECKO_SDK)/platform/Device/SiliconLabs/EFR32FG23/Source/system_efr32fg23.c \
+               $(GECKO_SDK)/platform/common/src/sl_assert.c \
+               $(GECKO_SDK)/platform/emlib/src/em_acmp.c \
+               $(GECKO_SDK)/platform/emlib/src/em_cryotimer.c \
+               $(GECKO_SDK)/platform/emlib/src/em_cmu.c \
+               $(GECKO_SDK)/platform/emlib/src/em_emu.c \
+               $(GECKO_SDK)/platform/emlib/src/em_eusart.c \
+               $(GECKO_SDK)/platform/emlib/src/em_gpio.c \
+               $(GECKO_SDK)/platform/emlib/src/em_letimer.c \
+               $(GECKO_SDK)/platform/emlib/src/em_pcnt.c \
+               $(GECKO_SDK)/platform/emlib/src/em_prs.c \
+               $(GECKO_SDK)/platform/emlib/src/em_lesense.c \
+               $(GECKO_SDK)/platform/emlib/src/em_lcd.c \
+               $(GECKO_SDK)/platform/emlib/src/em_system.c \
+               $(GECKO_SDK)/platform/emlib/src/em_vdac.c
 
-               #$(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/security/sl_component/sl_trustzone/src/sli_tz_service_syscfg_ns.c \
+               #$(GECKO_SDK)/platform/security/sl_component/sl_trustzone/src/sli_tz_service_syscfg_ns.c \
 
 EMLIBINC = \
                emlib \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/security/sl_component/sl_trustzone/inc/common \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/security/sl_component/sl_trustzone/inc/secure \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/security/sl_component/sl_trustzone/inc/nonsecure \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/util/third_party/trusted-firmware-m/interface/include \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/common/inc \
-               $(CHIBIOS_CONTRIB)/ext/gecko-sdk/platform/emlib/inc
+               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/common \
+               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/secure \
+               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/nonsecure \
+               $(GECKO_SDK)/util/third_party/trusted-firmware-m/interface/include \
+               $(GECKO_SDK)/platform/common/inc \
+               $(GECKO_SDK)/platform/emlib/inc
 
 # Shared variables
 ALLCSRC += $(EMLIBSRC)
