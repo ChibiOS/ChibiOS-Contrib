829be5ad9fa90feca373b60852404402bc2c02b0
Xael South <xael.south@yandex.com>
2024-06-09 22:50:29 +0200

    fixed 32768 if no subseconds

--- os/hal/ports/SILABS/LLD/EFR32FG23/BURTCv1/hal_rtc_lld.c
+++ os/hal/ports/SILABS/LLD/EFR32FG23/BURTCv1/hal_rtc_lld.c
@@ -66,8 +66,6 @@
 #error "EFR32_RTCCLK_DIV is not obtainable"
 #endif
 
-#define EFR32_RTCCLK              (EFR32_BURTCCLK / EFR32_RTCCLK_DIV)
-
 #define EFR32_OVERFLOW_SCALE(val)                                             \
   ((val) * ((_EFR32_OVERFLOW_VALUE - (EFR32_RTCCLK - 1U)) / EFR32_RTCCLK + 1U))
 
@@ -76,17 +74,17 @@
 #if EFR32_BURTCCLK % 32768U == 0U
 #define EFR32_RTCCLK_DIV 32768U
 #define EFR32_BURTC_CFG_CNTPRESC  BURTC_CFG_CNTPRESC_DIV32768
-
-#define EFR32_RTCCLK              (EFR32_BURTCCLK / EFR32_RTCCLK_DIV)
+#else
+#error "EFR32_RTCCLK_DIV is not obtainable"
+#endif
 
 /* Overflow handling can't be implemented here.
    After 68 years it just overflows. */
 #define EFR32_OVERFLOW_SCALE(val) ((val) * 0U)
-#else
-#error "EFR32_RTCCLK_DIV is not obtainable (resolution better than one millisec)"
-#endif
 
 #endif
+
+#define EFR32_RTCCLK              (EFR32_BURTCCLK / EFR32_RTCCLK_DIV)
 
 #define EFR32_RTC_STARTOFTIME     1980
 
