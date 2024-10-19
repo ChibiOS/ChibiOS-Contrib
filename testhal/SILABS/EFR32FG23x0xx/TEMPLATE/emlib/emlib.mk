b9ab5f724c68b73608ccf7c28370ceb3d684ab3f
Xael South <xael.south@yandex.com>
2024-10-20 01:00:14 +0200

    removed unnecessary includes

--- testhal/SILABS/EFR32FG23x0xx/TEMPLATE/emlib/emlib.mk
+++ testhal/SILABS/EFR32FG23x0xx/TEMPLATE/emlib/emlib.mk
@@ -3,7 +3,6 @@ GECKO_SDK = $(CHIBIOS_CONTRIB)/ext/gecko_sdk
 
 EMLIBSRC = \
                emlib/emlib_chibios.c \
-               $(GECKO_SDK)/platform/common/src/sl_assert.c \
                $(GECKO_SDK)/platform/emlib/src/em_acmp.c \
                $(GECKO_SDK)/platform/emlib/src/em_adc.c \
                $(GECKO_SDK)/platform/emlib/src/em_aes.c \
@@ -49,14 +48,9 @@ EMLIBSRC = \
 
 #EMLIBSRC += $(GECKO_SDK)/platform/emlib/src/em_core.c
 #EMLIBSRC += $(GECKO_SDK)/platform/Device/SiliconLabs/EFR32FG23/Source/system_efr32fg23.c
-#EMLIBSRC += $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/src/sli_tz_service_syscfg_ns.c
 
 EMLIBINC = \
                emlib \
-               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/common \
-               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/secure \
-               $(GECKO_SDK)/platform/security/sl_component/sl_trustzone/inc/nonsecure \
-               $(GECKO_SDK)/util/third_party/trusted-firmware-m/interface/include \
                $(GECKO_SDK)/platform/common/inc \
                $(GECKO_SDK)/platform/emlib/inc
 
