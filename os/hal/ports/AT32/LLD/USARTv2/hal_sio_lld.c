7e29212c2032b20986e356c9034024a7c8327f88
HorrorTroll <sonicvipduc@gmail.com>
2026-01-11 23:43:31 +0700

    Update ADC v1/2, USART v2, SDIO LLD drivers.

--- os/hal/ports/AT32/LLD/USARTv2/hal_sio_lld.c
+++ os/hal/ports/AT32/LLD/USARTv2/hal_sio_lld.c
@@ -1,7 +1,7 @@
 /*
     ChibiOS - Copyright (C) 2006..2018 Giovanni Di Sirio
-    ChibiOS - Copyright (C) 2023..2025 HorrorTroll
-    ChibiOS - Copyright (C) 2023..2025 Zhaqian
+    ChibiOS - Copyright (C) 2023..2026 HorrorTroll
+    ChibiOS - Copyright (C) 2023..2026 Zhaqian
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -553,7 +553,7 @@ sioevents_t sio_lld_get_events(SIODriver *siop) {
  * @param[in] buffer        pointer to the buffer for read frames
  * @param[in] n             maximum number of frames to be read
  * @return                  The number of frames copied from the buffer.
- * @retval 0                if the TX FIFO is full.
+ * @retval 0                if the RX FIFO is empty.
  */
 size_t sio_lld_read(SIODriver *siop, uint8_t *buffer, size_t n) {
   size_t rd;
@@ -732,7 +732,7 @@ void sio_lld_serve_interrupt(SIODriver *siop) {
     /* If there are no errors then we check for the other RX-related
        status flags.*/
     else {
-      /* Idle RX flag.*/
+      /* Idle RX flag. Note: At start the USART will produce an IDLEF interrupt.*/
       if ((sts & USART_STS_IDLEF) != 0U) {
 
         /* Interrupt source disabled.*/
